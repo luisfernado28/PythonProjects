58aa6904e6900ef2e52d56dc5b133e9424d33054
luisfernado28 <luisfer_2810@outlook.com>
2020-08-12 12:24:13 -0400

    Picking Numbers Problem

---
+++ HackerRank/medium/PickingNumbers.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    numbers=sorted(a)
+    maxvalue=0
+    cont=1
+    for i in range(1,len(numbers)):
+
+        if (numbers[i]-numbers[i-1])<2:
+            cont+=1
+        else:
+            if maxvalue<cont:
+                maxvalue=cont
+            cont=1
+    print(maxvalue)
+if __name__ == '__main__':
+
+
+    result = pickingNumbers([4,5,6,3,3,1])
+
