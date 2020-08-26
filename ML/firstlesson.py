ba48e723f5d0dbd2dc667d7c4407691989c4482d
luisfernado28 <luisfer_2810@outlook.com>
2020-08-25 20:42:32 -0400

    Machine learning Folder Created Libraries installed: Pandas, matplotlib, sklearn

---
+++ ML/firstlesson.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+#dataset = pd.read_csv()
+#dataset.head()
+#dataset.shape
+
+#selecciona columnas (inv)
+#x= dataset.iloc[:,:-1]
+#y= dataset.iloc[:,1]
